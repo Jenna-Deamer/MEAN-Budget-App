924ed3fac4d00eec440747d04cf9dccecc02fe33
Jenna Deamer <jennadeamer@gmail.com>
2024-04-26 15:57:02 -0400

    login/sign-up forms content & css, Fixed home hero button routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component'
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { TransactionsComponent } from './transactions/transactions.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+
+/**FORMS**/
+import { SignupComponent } from './forms/signup/signup.component';
+import { LoginComponent } from './forms/login/login.component';
+
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: '', component: HomeComponent },
     {path: 'transactions',component: TransactionsComponent},
+    {path: 'signup',component: SignupComponent},
+    {path: 'login',component: LoginComponent},
 ];
