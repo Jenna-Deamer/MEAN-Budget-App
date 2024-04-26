89df84f82c20b5145853995f45aef1b4eb6b609b
Jenna Deamer <jennadeamer@gmail.com>
2024-04-25 20:41:32 -0400

    home route fixed, Transcations table desktop view created & styled

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component'
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent },
+    {path: 'transactions',component: TransactionsComponent},
 ];
