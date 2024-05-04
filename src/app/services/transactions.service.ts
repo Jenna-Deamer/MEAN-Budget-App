631a3cbf8a54d601142d63ec115c88b658657f00
Jenna Deamer <jennadeamer@gmail.com>
2024-05-04 17:50:16 -0400

    updated url for api access

--- src/app/services/transactions.service.ts
+++ src/app/services/transactions.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environment/environment ';
 
 
 @Injectable({
   providedIn: 'root'
+  //Angular will create a single instance of this service for the entire application at root level.
 })
 export class TransactionsService {
 
   constructor(private http: HttpClient) { }
-
+  //set api url class level so all methods share it
+  serverUrl: string = environment.serverUrl;
   //GET
   getTransactions() {
     //use http to call express API
-    return this.http.get('http://localhost:3000/api/transactions');
+    return this.http.get(`${this.serverUrl}/api/transactions`);
   }
 }
---
+++ src/environment/environment .ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    serverUrl: "http://localhost:3000/v1"
+  };
+  
