56b2431ff8ca3e8d852840be483c36927d42aa64
Jenna Deamer <jennadeamer@gmail.com>
2024-05-11 18:07:05 -0400

    css & routes for goal crud

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,16 +8,20 @@ import { SignupComponent } from './components/forms/signup/signup.component';
 import { LoginComponent } from './components/forms/login/login.component';
 import { CreateComponent } from './components/forms/create/create.component';
 import { EditComponent } from './components/forms/edit/edit.component';
-/**UI-Components**/
-import { createComponent } from '@angular/core';
+/**Goal Forms**/
+import { CreateGoalComponent } from './components/forms/create-goal/create-goal.component';
+import { EditGoalComponent } from './components/forms/edit-goal/edit-goal.component';
 
+import { createComponent } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'dashboard', component: DashboardComponent },
-    {path: 'transactions',component: TransactionsComponent},
-    {path: 'signup',component: SignupComponent},
-    {path: 'login',component: LoginComponent},
-    {path: 'create', component: CreateComponent},
+    { path: 'transactions', component: TransactionsComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'create', component: CreateComponent },
     { path: 'edit/:id', component: EditComponent },
+    { path: 'create-goal', component: CreateGoalComponent },
+    { path: 'edit-goal/:id', component: EditGoalComponent }
 ];
